93206dc8bd226b1b711ba0f4fd349d8e1cb96d75
Aurélien Riche <contact@aurelienriche.com>
2021-03-25 18:03:47 +0100

    fix 1

---
+++ build/precache-manifest.8d4a5e22769ae3d64acd886a4cbc0459.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "473cd542a14ddeb1a0731cce91292778",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d6641e70566d7f23c18b",
+    "url": "/static/css/2.b0704b7a.chunk.css"
+  },
+  {
+    "revision": "6fc3662118c91205d994",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "d6641e70566d7f23c18b",
+    "url": "/static/js/2.7885ab19.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7885ab19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fc3662118c91205d994",
+    "url": "/static/js/main.e87b5443.chunk.js"
+  },
+  {
+    "revision": "48174c8cd5b2215a61e7",
+    "url": "/static/js/runtime-main.4c77bff9.js"
+  }
+]);
