2bd9c373b4e3f78118cd7acd45e0bdef5456aacb
Aurélien Riche <contact@aurelienriche.com>
2021-04-13 16:39:52 +0200

    Fix cors issues

--- src/services/upload-files.service.js
+++ src/services/upload-files.service.js
@@ -1,22 +1,23 @@
 import http from "../http-common";
 
 class UploadFilesService {
-  upload(file, onUploadProgress) {
-    let formData = new FormData();
+    upload(file, onUploadProgress) {
+        let formData = new FormData();
 
-    formData.append("file", file);
+        formData.append("file", file);
 
-    return http.post("/upload", formData, {
-      headers: {
-        "Content-Type": "multipart/form-data",
-      },
-      onUploadProgress,
-    });
-  }
+        return http.post("http://localhost:8000/upload", formData, {
+            headers: {
+                "Content-Type": "multipart/form-data",
+                "Access-Control-Allow-Origin": "*",
+            },
+            onUploadProgress,
+        });
+    }
 
-  getFiles() {
-    return http.get("/files");
-  }
+    getFiles() {
+        return http.get("/files");
+    }
 }
 
 export default new UploadFilesService();
